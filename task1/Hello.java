7bebd9dcffa1fc396b60c18e1aee36a2e01787fa
ravikumarr1802 <ravikumarrangu2@gmail.com>
2025-06-30 23:00:48 +0530

    Code with errors

--- task1/Hello.java
+++ task1/Hello.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Hello {
 
     public static void main(String[] args) {
-        // TO DO: Write your code below 
-        
+        // TO DO: Write your code below
+        System.out.println("Hello, World!");
     }
 }
